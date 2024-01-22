f06e7963686f39fea223b6188a20a07bc297cf93
Dwayne Edwards <edwardsdl@jncb.com>
2024-01-22 17:24:00 -0500

    Update specialization

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,6 +34,7 @@ const Layout = ({ children }) => {
             <div className="col-lg-3 col-md-4 mx-auto">
                <div><h6>Contact</h6></div>
                <div className="py-1"><StaticImage src="../images/envelope-at-fill.svg" alt="Send email to kcdlawfirm@gmail.com" width={20}/> kcdlawfirm@gmail.com</div>
+               <div className="py-1"><StaticImage src="../images/envelope-at-fill.svg" alt="Send email to karliacartydinnall@gmail.com" width={20}/> karliacartydinnall@gmail.com</div>
                <div className="py-1"><StaticImage src="../images/telephone-inbound-fill.svg" alt="Send email to kcdlawfirm@gmail.com" width={20}/> +1-876-546-7382</div>
             </div>
             <div className="col-lg-3 col-md-4 mx-auto">
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
-import { title, profile, credit } from "../components/index.module.css"
+import { title, profile } from "../components/index.module.css"
 
 
 const IndexPage = () => (
@@ -16,25 +16,46 @@ const IndexPage = () => (
         </div>
         <div className="col-lg-10 col-md-10 py-4">
           <h1 className={title}>KARLIA CARTY-DINNALL</h1>
-          <h2>ATTORNEY-AT-LAW & MEDIATOR</h2>        
+          <h2>ATTORNEY-AT-LAW & MEDIATOR</h2>         
         </div>
       </div> 
-
       <div className="row bg-dark text-white-50">
-        <h4>Practice Areas</h4>
+          <h4 id="practice">Specializations</h4>
       </div>
-      <div className="row py-5">
+      <div className="row py-4">
         <div className="col-lg-3 col-md-4 mx-auto">
           <div className="card">
-            <div className="card-header bg-warning fw-bold">Family</div>
+            <div className="card-header bg-warning fw-bold">Divorce</div>
             <StaticImage src="../images/family.jpg" class="card-img-top" alt="Image of family"/>
-            <p className={credit}>Photo Credit: <a href="https://unsplash.com/photos/a-man-and-two-women-holding-a-child-on-their-shoulders-4ardiyvfhTM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash">Osarugue Igbinoba</a></p>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 mx-auto">
+          <div className="card">
+            <div className="card-header bg-warning fw-bold">Property Sales & Purchases</div>
+            <StaticImage src="../images/real_estate_legal.jpg" class="card-img-top" alt="Image of family"/>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 mx-auto">
+          <div className="card">
+            <div className="card-header bg-warning fw-bold">Probate & Administration of Estate</div>
+            <StaticImage src="../images/probate.jpg" class="card-img-top" alt="Image of family"/>
+          </div>
+        </div>
+      </div>
+      <div className="row bg-dark text-white-50">
+          <h4 id="practice">Practice Areas</h4>
+      </div>
+      <div className="row py-4">
+        <div className="col-lg-3 col-md-4 mx-auto">
+          <div className="card">
+            <div className="card-header bg-warning fw-bold">Probate &amp; Estate Administration</div>
+            <StaticImage src="../images/litigation.jpg" class="card-img-top" alt="Image of court room"/>
             <div className="card-body">
-            <ul className="list-group list-group-flush py-2">
-                <li className="list-group-item">Divorce</li>
-                <li className="list-group-item">Custody & Maintenance</li>
-                <li className="list-group-item">Marital Property Disputes</li>
-                <li className="list-group-item">Family Law Litigation</li>
+              <ul className="list-group list-group-flush py-2">
+                <li className="list-group-item">Citations</li> 
+                <li className="list-group-item">Drafting of Wills</li>
+                <li className="list-group-item">Grant of Probate</li>
+                <li className="list-group-item">Letters of Administration</li>                               
               </ul>
               <Link to="/contact"><button className="btn btn-dark text-white-50" type="submit">Book Consultation</button></Link>
             </div>
@@ -42,15 +63,35 @@ const IndexPage = () => (
         </div>
         <div className="col-lg-3 col-md-4 mx-auto">
           <div className="card">
-            <div className="card-header bg-warning fw-bold">General Legal Services</div>
+            <div className="card-header bg-warning fw-bold">Family</div>
+            <StaticImage src="../images/family.jpg" class="card-img-top" alt="Image of family"/>
+            <div className="card-body">
+            <ul className="list-group list-group-flush py-2">
+                <li className="list-group-item">Divorce</li>
+                <li className="list-group-item">Custody</li>                
+                <li className="list-group-item">Division</li>
+                <li className="list-group-item">Property Rights</li>
+                <li className="list-group-item">Child Maintenance</li>
+                <li className="list-group-item">Spousal Maintenance</li> 
+              </ul>
+              <Link to="/contact"><button className="btn btn-dark text-white-50" type="submit">Book Consultation</button></Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-4 mx-auto">
+          <div className="card">
+            <div className="card-header bg-warning fw-bold">Civil &amp; Commercial Litigation</div>
             <StaticImage src="../images/legal-services.jpg" class="card-img-top" alt="Image of lady justice"/>
-            <p className={credit}>Photo Credit: <a href="https://unsplash.com/photos/woman-in-gold-dress-holding-sword-figurine-L4YGuSg0fxs?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash">Tingey Injury Law Firm</a></p>
             <div className="card-body">
               <ul className="list-group list-group-flush py-2">
-                <li className="list-group-item">Contracts, Agreements & Deeds</li>
-                <li className="list-group-item">Company & Business Registration</li>
-                <li className="list-group-item">Demand Letters & Notices</li>
-                <li className="list-group-item">Other General Legal Services</li>
+                <li className="list-group-item">Injunctions</li>
+                <li className="list-group-item">Labour &amp; Employment</li>
+                <li className="list-group-item">Landlord &amp; Tenant</li>
+                <li className="list-group-item">Construction</li>
+                <li className="list-group-item">Personal Injury</li>                
+                <li className="list-group-item">Power of Attorney Deeds</li>
+                <li className="list-group-item">Partnership Agreements</li>
+                <li className="list-group-item">Drafting of Contracts and Deeds</li>
               </ul>
               <Link to="/contact"><button className="btn btn-dark text-white-50" type="submit">Book Consultation</button></Link>
             </div>
@@ -58,31 +99,17 @@ const IndexPage = () => (
         </div>
         <div className="col-lg-3 col-md-4 mx-auto">
           <div className="card">
-            <div className="card-header bg-warning fw-bold">Civil & Commercial Litigation</div>
-            <StaticImage src="../images/litigation.jpg" class="card-img-top" alt="Image of court room"/>
-            <p className={credit}>Photo Credit: <a href="https://unsplash.com/photos/brown-wooden-chairs-inside-room-1YeQl23dvJI?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash">Kelly Sikkema</a></p>
-            <div className="card-body">
-              <ul className="list-group list-group-flush py-2">
-                <li className="list-group-item">Debt Claims</li>
-                <li className="list-group-item">Property Disputes</li>
-                <li className="list-group-item">Personal Injury</li>
-                <li className="list-group-item">Breach of Contract</li>                
-              </ul>
-              <Link to="/contact"><button className="btn btn-dark text-white-50" type="submit">Book Consultation</button></Link>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 mx-auto">
-          <div className="card">
-            <div className="card-header bg-warning fw-bold">Real Estate</div>
+            <div className="card-header bg-warning fw-bold">Real Estate &amp; Conveyancing</div>
             <StaticImage src="../images/real-estate.jpg" class="card-img-top" alt="Image of home"/>
-            <p className={credit}>Photo Credit: <a href="https://unsplash.com/photos/white-concrete-residential-building-near-trees-CLKGGwIBTaY?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash">Bernard Hermant</a></p>
             <div className="card-body">
               <ul className="list-group list-group-flush py-2">
-                <li className="list-group-item">Property Transactions</li>
-                <li className="list-group-item">Sale & Lease Agreements</li>
-                <li className="list-group-item">Land & Property Disputes</li>
-                <li className="list-group-item">Landlord & Tenant Disputes</li>
+              <li className="list-group-item">Caveats</li>
+                <li className="list-group-item">Transmission</li>
+                <li className="list-group-item">Adverse Possession</li>                
+                <li className="list-group-item">Registration of Title</li>
+                <li className="list-group-item">Property Development</li>
+                <li className="list-group-item">Restrictive Covenants and Modifications</li>
+                <li className="list-group-item">Land Title Matters including Lost Title Applications</li>     
               </ul>
               <Link to="/contact"><button className="btn btn-dark text-white-50" type="submit">Book Consultation</button></Link>
             </div>
